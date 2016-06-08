20e6f8bc083a7d6db620336db8a58c8e5572ee00
Revill1 <adamredlinski@gmail.com>
2016-06-09 00:09:10 +0200

    added first test for public news

--- src/main/java/edu/iis/mto/staticmock/PublishableNews.java
+++ src/main/java/edu/iis/mto/staticmock/PublishableNews.java
@@ -13,13 +13,21 @@ public class PublishableNews {
 	private final List<String> subscribentContent = new ArrayList<>();
 
 	public void addPublicInfo(String content) {
-		this.publicContent .add(content);
+		this.getPublicContent() .add(content);
 		
 	}
 
 	public void addForSubscription(String content, SubsciptionType subscriptionType) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public List<String> getSubscribentContent() {
+		return subscribentContent;
+	}
+
+	public List<String> getPublicContent() {
+		return publicContent;
 	}
 
 }
--- src/test/java/lab3_2/TestLoadNews.java
+++ src/test/java/lab3_2/TestLoadNews.java
@@ -2,6 +2,64 @@ package lab3_2;
 
 import static org.powermock.api.mockito.PowerMockito.*;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import edu.iis.mto.staticmock.Configuration;
+import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.IncomingInfo;
+import edu.iis.mto.staticmock.IncomingNews;
+import edu.iis.mto.staticmock.NewsLoader;
+import edu.iis.mto.staticmock.NewsReaderFactory;
+import edu.iis.mto.staticmock.PublishableNews;
+import edu.iis.mto.staticmock.SubsciptionType;
+import edu.iis.mto.staticmock.reader.NewsReader;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationLoader.class , NewsReaderFactory.class})
 public class TestLoadNews {
 
+	private ConfigurationLoader confLoader;
+	private NewsReaderFactory factory;
+	private NewsReader reader;
+	private NewsLoader loader;
+	
+	
+	@Before
+	public void start()
+	{
+		confLoader = mock(ConfigurationLoader.class);
+		when(confLoader.loadConfiguration()).thenReturn(new Configuration());
+		mockStatic(ConfigurationLoader.class);
+		when(ConfigurationLoader.getInstance()).thenReturn(confLoader);
+
+		reader = mock(NewsReader.class);
+		when(reader.read()).thenReturn(new IncomingNews());
+
+		mockStatic(NewsReaderFactory.class);
+		when(NewsReaderFactory.getReader(Mockito.anyString())).thenReturn(reader);
+
+		loader = new NewsLoader();
+	}
+	
+	@Test
+	public void newsTest()
+	{
+		IncomingNews incomingNews = new IncomingNews();
+		incomingNews.add(new IncomingInfo("type a",SubsciptionType.A));
+		incomingNews.add(new IncomingInfo("type b",SubsciptionType.B));
+		incomingNews.add(new IncomingInfo("type c",SubsciptionType.NONE));
+		incomingNews.add(new IncomingInfo("type c",SubsciptionType.NONE));
+		
+		when(reader.read()).thenReturn(incomingNews);
+		
+		PublishableNews news = loader.loadNews();
+		Assert.assertEquals(news.getPublicContent().size(), 2);
+	}
+	
 }
